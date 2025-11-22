b6c29ea5d1a4594662d774a2d12e6c0b45c58b8b
DrWarpMan <drwarpman@gmail.com>
2025-11-22 17:43:26 +0100

    set max game limit

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const configSchema = z.array(
 			.min(1)
 			.regex(/^[a-zA-Z0-9_.@]+$/),
 		password: z.string().min(1),
-		games: z.array(z.number().int().positive()).min(1),
+		games: z.array(z.number().int().positive()).min(1).max(32),
 		online: z.boolean().default(false),
 	}),
 );
